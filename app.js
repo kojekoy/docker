fe2f9370d355053668af0da9af8e00d18837eeb2
kojekoy <32273734+kojekoy@users.noreply.github.com>
2020-03-26 21:30:48 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //UPDATE atau INSERT kedealam DATABSE
 var mysql = require("mysql");
-const osutils = require("os-utils");
+//const osutils = require("os-utils");
+var os 	= require('os-utils');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var cron = require('node-cron');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
 app.listen(3000);
 // Connection Configurations
 var con = mysql.createConnection({
-    host: '192.168.137.100',
+    host: 'localhost',
     user: 'root',
     password: '123',
     database: 'tesdb'
@@ -28,36 +28,22 @@ app.get('/data', function (req, res) {
     con.query('SELECT * FROM docker', function (error, results, fields) {
         if (error) throw error;
         return res.send({ error: false, data: results, message: 'DATA HEALTYCHECK' });
+      });
     });
-});
-//INPUT TO DATABASE
-cron.schedule('* * * * *', () => {
+//Execute To DATABASE
+app.get('/update',function(req,res){
 var mem= osutils.totalmem();
 var fmem= osutils.freemem();
 var cpu= osutils.freememPercentage();
 var id = 1
-
-//Coonect To DATABSE
-var con = mysql.createConnection({
-  host: "192.168.137.100",
-  user: "root",
-  password: "123",
-  database: "tesdb"
-});
-con.connect(function(err){
-  if(err){
-    console.log('Error connecting to Db');
-    return;
-  }
-  console.log('Connection established');
-});
-
-//Execute To DATABASE
-//var sql = "INSERT INTO docker (memt, memf, cpu) VALUES ('"+mem+"', '"+fmem+"','"+cpu+"')";
-var sql = "UPDATE docker set memt=?, memf=?, cpu=? WHERE id=?"
-con.query(sql,[mem,fmem,cpu,id], function (err, result) {
-  if (err) throw err;
-  console.log("UPDATE to, ID: " + id);
-      });
-});
-
+//var mem= os.totalmem();
+//var fmem= os.freemem();
+//var cpu= os.freememPercentage();
+   // var sql = "INSERT INTO docker (memt, memf, cpu) VALUES ('"+mem+"', '"+fmem+"','"+cpu+"')";
+    var sql = "UPDATE docker set memt=?, memf=?, cpu=? WHERE id=?"
+    con.query(sql,[mem,fmem,cpu,id], function (err, result) {
+        if (err) throw err;
+        console.log("UPDATE to, ID: " + id);
+res.redirect('/')
+     });
+    });
